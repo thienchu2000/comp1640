ac6ddb045f8418a44dab7ea67952450cc61faea6
mac <mac@192.168.2.252>
2024-01-15 12:57:37 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
+});
